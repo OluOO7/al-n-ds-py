229efbc59c22f02e199f8bdd80469d38fb1025d9
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2021-03-02 06:58:48 +0000

    Programming exercise and Proper Class Definition

--- oop-defining-classes.py
+++ oop-defining-classes.py
@@ -20,9 +20,13 @@ print(lcm(6,9))
 
 class Fraction:
     """Fraction Class"""
-    def __init__(self, top, bottom):        #first method of any class, the Constructor that defines
-        self.num = top                      #the initial internal states of a class instance/member.
-        self.den = bottom
+    def __init__(self, top, bottom):                #first method of any class, the Constructor that defines
+        if top % 1 != 0 or bottom % 1 != 0:        #the initial internal states of a class instance/member.
+            raise RuntimeError("Enter integer parameters") 
+        else:                                               
+            common_divisor = gcd(top, bottom)
+            self.num = top // common_divisor
+            self.den = bottom // common_divisor
     
     def show(self):                         #show() method that prints the internal states of a class. 
         print(f"{self.num}/{self.den}")     #instance. 
@@ -34,9 +38,6 @@ class Fraction:
         result_num = self.num * other_fraction.den + \
                      other_fraction.num * self.den
         result_den = self.den * other_fraction.den
-        common_divisor = gcd(result_num,result_den)
-        result_num //= common_divisor
-        result_den //= common_divisor
         result = Fraction(result_num, result_den)
         return result
 
@@ -44,9 +45,6 @@ class Fraction:
         result_num = self.num * other_fraction.den - \
                      other_fraction.num * self.den
         result_den = self.den * other_fraction.den
-        common_divisor = gcd(result_num,result_den)
-        result_num //= common_divisor
-        result_den //= common_divisor
         result = Fraction(result_num, result_den)
         return result
     
@@ -68,25 +66,19 @@ class Fraction:
     def __mul__(self, other_fraction):      
         num_product = self.num * other_fraction.num
         den_product = self.den * other_fraction.den
-        common_divisor = gcd(num_product, den_product)
-        result_num = num_product // common_divisor
-        result_den = den_product // common_divisor
-        return Fraction(result_num, result_den)
+        return Fraction(num_product, den_product)
 
     def __truediv__(self, other_fraction):      
         cross_product_1 = self.num * other_fraction.den
         cross_product_2 = self.den * other_fraction.num
-        common_divisor = gcd(cross_product_1, cross_product_2)
-        result_num = cross_product_1 // common_divisor
-        result_den = cross_product_2 // common_divisor
-        return Fraction(result_num, result_den)
+        return Fraction(cross_product_1, cross_product_2)
 
 a_fraction = Fraction(2,5)                  #an instance of Fraction class with states(num = 2 and den = 5).
 a_fraction.show()  
 print(a_fraction)     
 a_fraction.__str__()
-a = Fraction(5,8)
-b = Fraction(1,4)
+a = Fraction(2,8)
+b = Fraction(3,12)
 print(a + b)
 print(a == b)
 print(a * b)
---
+++ sudoku-master.py
@@ -0,0 +1,1 @@
+#Program solves sudoku.
